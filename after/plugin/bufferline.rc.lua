3069025deb983205afbf4617d0e2e9645d4d65e1
themuuln <zerone.offical@gmail.com>
2022-11-21 18:25:19 +0800

    🪶 --bufferline look fixed

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -11,19 +11,11 @@ bufferline.setup({
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		color_icons = true,
-
-		diagnostics_indicator = function(count, level, diagnostics_dict, context)
-			local s = " "
-			for e, n in pairs(diagnostics_dict) do
-				local sym = e == "error" and " " or (e == "warning" and " " or "")
-				s = s .. n .. sym
-			end
-			return s
-		end,
 	},
 	highlights = {
 		separator = {
 			fg = "#282c34",
+			bg = "#282c34",
 		},
 		separator_selected = {
 			fg = "#282c34",
@@ -35,6 +27,9 @@ bufferline.setup({
 		buffer_selected = {
 			fg = "#fdf6e3",
 			bold = true,
+		},
+		fill = {
+			bg = "#282c34",
 		},
 	},
 })
